9cec763532b7716ef873114468eb314aa23d93c3
Elina Pakharevskaya <elina.paxar@gmail.com>
2024-04-19 00:35:02 +0300

    create handler logic (#13)

--- piglet-auth/internal/grpc/auth/server.go
+++ piglet-auth/internal/grpc/auth/server.go
@@ -3,34 +3,127 @@ package auth
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	authv1 "piglet-auth-service/api/proto/gen"
 )
 
-type serverAPI struct {
-	authv1.UnimplementedAuthServer
+type Auth interface {
+	Login(
+		ctx context.Context,
+		username string,
+		password string,
+	) (token string, err error)
+	Register(
+		ctx context.Context,
+		username string,
+		email string,
+		password string,
+	) (userID int64, err error)
+	UpdateUser(
+		ctx context.Context,
+		username string,
+		email string,
+		oldPassword string,
+		password string,
+	) (success bool, err error)
 }
 
-func Register(gRPC *grpc.Server) {
-	authv1.RegisterAuthServer(gRPC, &serverAPI{})
+type serverAPI struct {
+	authv1.UnimplementedAuthServer
+	auth Auth
+}
+
+func Register(gRPC *grpc.Server, auth Auth) {
+	authv1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *authv1.LoginRequest,
 ) (*authv1.LoginResponse, error) {
-	panic("Login: implement me")
+	// TODO: setup validation
+	if req.GetUsername() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "username is empty")
+	}
+	if req.GetPass() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "password is empty")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetUsername(), req.GetPass())
+	if err != nil {
+		// TODO ...
+		return nil, status.Errorf(codes.Unauthenticated, "invalid credentials")
+	}
+
+	return &authv1.LoginResponse{
+		Token: token,
+	}, nil
 }
 
 func (s *serverAPI) Register(
 	ctx context.Context,
 	req *authv1.RegisterRequest,
 ) (*authv1.RegisterResponse, error) {
-	panic("Register: implement me")
+	// TODO: setup validation
+	if req.GetUsername() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "username is empty")
+	}
+	if req.GetEmail() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "email is empty")
+	}
+	if req.GetPass() == "" || req.GetConPass() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "password is empty")
+	}
+	if req.GetPass() != req.GetConPass() {
+		return nil, status.Errorf(codes.InvalidArgument, "password mismatch")
+	}
+
+	userID, err := s.auth.Register(ctx, req.GetUsername(), req.GetEmail(), req.GetPass())
+	if err != nil {
+		// TODO ...
+		return nil, status.Errorf(codes.Unauthenticated, "invalid credentials")
+	}
+
+	return &authv1.RegisterResponse{
+		UserId: userID,
+	}, nil
 }
 
 func (s *serverAPI) UpdateUser(
 	ctx context.Context,
 	req *authv1.UpdateUserRequest,
 ) (*authv1.UpdateUserResponse, error) {
-	panic("Update: implement me")
+	// TODO: setup validation
+	if req.GetUsername() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "username is empty")
+	}
+	if req.GetEmail() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "email is empty")
+	}
+	if req.GetOldPass() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "old password is empty")
+	}
+	if req.GetPass() == "" || req.GetConPass() == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "password is empty")
+	}
+	if req.GetPass() != req.GetConPass() {
+		return nil, status.Errorf(codes.InvalidArgument, "password mismatch")
+	}
+
+	success, err := s.auth.UpdateUser(
+		ctx,
+		req.GetUsername(),
+		req.GetEmail(),
+		req.GetOldPass(),
+		req.GetPass(),
+	)
+	if err != nil {
+		// TODO ...
+		return nil, status.Errorf(codes.Unauthenticated, "invalid credentials")
+	}
+
+	return &authv1.UpdateUserResponse{
+		Success: success,
+	}, err
 }
