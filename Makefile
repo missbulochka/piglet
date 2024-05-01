6afa31ecba43d1dd94164df4ffc42974244136a8
Elina Pakharevskaya <elina.paxar@gmail.com>
2024-05-01 16:43:50 +0300

    update some automation (add images building)

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 # Generate all Client and Server code using proto file
 generate-pb-go:
 	./piglet-bills/scripts/gen_grpc.sh
+
+# You can build all images
+build-all:
+	./piglet-bills/scripts/build_images.sh
---
+++ piglet-bills/scripts/build_images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# You can build docker-image base (*.build.Dockerfile). This image is common for all services.
+docker build \
+        -t piglet_base:0.1.0 \
+        -f piglet-bills/.devcontainer/base.build.Dockerfile \
+        ./
+
+# You can build docker-image (*.Dockerfile)
+docker build \
+        -t piglet-bills:0.1.0 \
+        -f piglet-bills/docker/piglet-bills.Dockerfile \
+        ./
