4246a970845986fce58fba8c46a0cb79bcc445a8
Elina Pakharevskaya <elina.paxar@gmail.com>
2024-05-13 13:55:58 +0300

    setup migratins (#31)

--- piglet-transactions/go.mod
+++ piglet-transactions/go.mod
@@ -3,12 +3,17 @@ module piglet-transactions-service
 go 1.21.3
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	google.golang.org/grpc v1.63.2
 	google.golang.org/protobuf v1.34.1
 )
 
 require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- piglet-transactions/internal/app/app.go
+++ piglet-transactions/internal/app/app.go
@@ -1,10 +1,12 @@
 package app
 
 import (
+	"fmt"
 	"log/slog"
 
 	grpcapp "piglet-transactions-service/internal/app/grpc"
 	"piglet-transactions-service/internal/config"
+	migrator "piglet-transactions-service/storage/pg-migration"
 )
 
 type App struct {
@@ -12,13 +14,23 @@ type App struct {
 }
 
 func New(log *slog.Logger, cfg *config.Config) *App {
-	// TODO: run migration
+	if err := migrator.RunMigration(
+		log,
+		"file://"+cfg.DB.MigrationPath,
+		fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+			cfg.DB.UserName,
+			cfg.DB.Password,
+			cfg.DB.DBHost,
+			cfg.DB.DBPort,
+			cfg.DB.DBName,
+		),
+	); err != nil {
+		panic(err)
+	}
 
 	// TODO: connect data base
 
 	// TODO: add service
-
-	// TODO: setup grpc-server
 
 	grpcApp := grpcapp.New(log, cfg.GRPC.GRPCServer, cfg.GRPC.GRPCPort)
 
