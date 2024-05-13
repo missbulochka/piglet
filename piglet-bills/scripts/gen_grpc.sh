62f60534803aaf25ea4ad01e03c3e8878beb46bd
Elina Pakharevskaya <elina.paxar@gmail.com>
2024-05-14 02:48:18 +0300

    move proto-files outside the project

--- piglet-bills/scripts/gen_grpc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate Client and Server code for bills-service using proto file
-mkdir -p api/proto/gen
-protoc -I=. \
-    --go_out=. \
-    --go-grpc_out=. \
-    api/proto/accounting.proto
